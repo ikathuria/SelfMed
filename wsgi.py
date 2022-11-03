4b0c49cdfcb66724db7f8bfc9d2626419027cb4e
ikathuria <ishani@kathuria.net>
2022-11-03 09:50:53 +0530

    heroku update

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,5 @@
-import sys
-project_home = '/home/ikathuria/selfmed'
-if project_home not in sys.path:
-    sys.path = [project_home] + sys.path
-
 from index import app as application
+
+
+if __name__ == "__main__":
+    application.run()
